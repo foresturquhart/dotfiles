ea367489c1507a66e71b8f302322a98e93004cea
Forest Urquhart <forest.urquhart@gmail.com>
2021-08-19 17:50:27 +0100

    Add additional public configuration files

--- .zshrc
+++ .zshrc
@@ -1,5 +1,13 @@
 [[ -r "${HOME}/.cache/p10k-instant-prompt-${(%):-%n}.zsh" ]] && source "${HOME}/.cache/p10k-instant-prompt-${(%):-%n}.zsh"
 
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
+export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}";
+export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
+
+export PATH="${HOME}/.composer/vendor/bin:${HOME}/bin:${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:${PATH}"
+
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env zsh
 
-mkdir -p ~/.gnupg
+touch ~/.hushlogin
+
+cp .gitignore ~/.gitignore
+cp .gitconfig ~/.gitconfig
+cp .dockerignore ~/.dockerignore
+
+cp .zshrc ~/.zshrc
+
+cp .curlrc ~/.curlrc
+cp .editorconfig ~/.editorconfig
+cp .inputrc ~/.inputrc
+cp .screenrc ~/.screenrc
+cp .tmux.conf ~/.tmux.conf
+cp .wgetrc ~/.wgetrc
+
+cp -R .ssh/ ~/.ssh
+
+cp -R .gnupg/ ~/.gnupg
 chown -R $(whoami) ~/.gnupg/
 chmod 700 ~/.gnupg
-cp .gnupg ~/.gnupg/gpg.conf
 chmod 600 ~/.gnupg/*
+
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
+/usr/local/bin/gpg --import < gpg.pub
